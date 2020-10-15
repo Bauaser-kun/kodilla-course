ec2f8d5cbe98924b01512d4aae5117ae01a562f1
Bauaser <michal.bala.1989@gmail.com>
2020-10-15 12:51:50 +0200

    6.2 pierwszy test jednostkowy - kalkulator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        double checkCalculator = calculator.adding(10.5, 2.0);
+
+        if (checkCalculator == 12.5) {
+            System.out.println("Calculator add test OK");
+        } else {
+            System.out.println("Calculator add Error!");
+        }
+
+        checkCalculator = calculator.substracting(10.5, 2.0);
+
+        if (checkCalculator == 8.5) {
+            System.out.println("Calculator substract test OK");
+        } else {
+            System.out.println("Calculator substract Error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public static double substracting (double a, double b){
+        return a-b;
+    }
+
+    public static double adding (double a, double b){
+        return a+b;
+    }
+}
