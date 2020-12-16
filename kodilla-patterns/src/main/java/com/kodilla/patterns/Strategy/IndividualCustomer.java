2f7abf78c64ea9eb785e9174ef8a57207ff1257d
Bauaser <michal.bala.1989@gmail.com>
2020-12-16 14:59:16 +0100

    Module 15.2 Example (investing strategies) added

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/Strategy/IndividualCustomer.java
@@ -0,0 +1,10 @@
+package com.kodilla.patterns.Strategy;
+
+import com.kodilla.patterns.Strategy.predictors.ConservativePredictor;
+
+public class IndividualCustomer extends Customer {
+    public IndividualCustomer(String name) {
+        super(name);
+        this.buyPredictor = new ConservativePredictor();
+    }
+}
